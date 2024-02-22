927184a337e5d814ed002454e934030237366aed
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-02-22 23:36:29 +0900

    Title: n개의 숫자 중 최대 2개, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240222/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int tmp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        System.out.printf("%d %d", arr[0], arr[1]);
+    }
+}
