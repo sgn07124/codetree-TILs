61de3b3ced799f78f6f41bb4ecacb06d5ba06b11
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-02-24 19:46:03 +0900

    Title: 문자열 두번 출력하기, Time: 221ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240224/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        for (int i = 0; i < 2; i++) {
+            System.out.println(a);
+        }
+    }
+}
