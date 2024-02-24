ed80faebfb2ddf2d6e17cb354e079a007a6c5bfb
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-02-24 19:52:27 +0900

    Title: 더 긴 문자열, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240224/더 긴 문자열/longer-string.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        if(a.length() == b.length()) {
+            System.out.print("same");
+        } else {
+            if (a.length() > b.length()) {
+                System.out.print(a + " " + a.length());
+            } else {
+                System.out.print(b + " " + b.length());
+            }
+        }
+    }
+}
