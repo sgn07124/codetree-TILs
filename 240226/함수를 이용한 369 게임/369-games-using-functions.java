505433ad4650455f5edb0072c63953493ff2ab1c
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-02-26 01:50:59 +0900

    Title: 함수를 이용한 369 게임, Time: 257ms, Memory: 20MB, Status: Passed - CodeTree

--- 240226/함수를 이용한 369 게임/369-games-using-functions.java
+++ 240226/함수를 이용한 369 게임/369-games-using-functions.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean isContains(int i) {
-        return i/10==3 || i/10==6 || i/10==9 || i%10==3 || i%10==6 || i%10==9;
+    public static boolean isContains(int t) {
+        String num = Integer.toString(t);
+        boolean a = false;
+        for(int i = 0;i < num.length(); i++){
+            if(num.charAt(i)=='3' || num.charAt(i) == '6' || num.charAt(i) == '9') {
+                a = true;
+                break;
+            }
+        }
+        return a;
     }
     public static boolean isThree(int i) {
         return i%3==0;
