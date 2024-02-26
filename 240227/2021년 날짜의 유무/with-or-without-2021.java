94cf0166ee5628a375fc07edba4302058ac34c58
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-02-27 02:34:20 +0900

    Title: 2021년 날짜의 유무, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240227/2021년 날짜의 유무/with-or-without-2021.java
+++ 240227/2021년 날짜의 유무/with-or-without-2021.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
 
-public class Main {
-    public static boolean func(int m, int d) {
-        if (m==2 && d>= 1 && d>= 29) {
-            return false;
-        } else if (m >= 1 && m <= 12 && d >= 1 && d <= 31) {
+public class Main {    
+    // 윤년이 아닐 때 m번째 달의 마지막 날을 반환하는 함수를 작성합니다.
+    public static int lastDayNumber(int m) {
+        if(m == 2)
+            return 28;
+        if(m == 4 || m == 6 || m == 9 || m == 11)
+            return 30;
+        
+        return 31;
+    }
+    
+    // 윤년이 아닐 때 m월 d일이 존재하는지 여부를 확인하는 함수를 작성합니다.
+    public static boolean judgeDay(int m, int d) {
+        if(m <= 12 && d <= lastDayNumber(m))
             return true;
-        }
+        
         return false;
     }
 
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int D = sc.nextInt();
-
-        if (func(M, D)) {
+        // 변수 선언 및 입력:
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        
+        if(judgeDay(m, d))
             System.out.print("Yes");
-        } else {
+        else
             System.out.print("No");
-        }
     }
 }
