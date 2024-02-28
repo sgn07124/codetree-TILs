ee185eb10f30177d0fc643a3996f8adf08ca0da3
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-02-28 18:15:54 +0900

    Title: 단어로 구분하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240228/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String arr [] = s.split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
