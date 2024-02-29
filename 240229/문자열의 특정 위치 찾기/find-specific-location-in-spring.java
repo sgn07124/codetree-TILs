43b288527d7af29246398ddc964d0d2c3ad8cd10
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-02-29 16:08:54 +0900

    Title: 문자열의 특정 위치 찾기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240229/문자열의 특정 위치 찾기/find-specific-location-in-spring.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int c = sc.next().charAt(0);
+        int n = s.indexOf(c);
+        if (n>0) {
+            System.out.print(n);
+        } else {
+            System.out.print("No");
+        }
+    }
+}
