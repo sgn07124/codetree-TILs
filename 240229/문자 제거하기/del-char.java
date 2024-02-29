0c2528ff01c24eb00f29445c7f70cbcb2984ada1
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-02-29 17:37:52 +0900

    Title: 문자 제거하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240229/문자 제거하기/del-char.java
+++ 240229/문자 제거하기/del-char.java
@@ -7,19 +7,18 @@ public class Main {
         String s = sc.next();
         char [] arr = s.toCharArray();
         int cnt = s.length();
-        while (true) {
+        for (int i = 0; i < cnt+2; i++) {
             int a = sc.nextInt();
             int sL = s.length();
             if (a > s.length()) {
-                s =s.substring(0, sL-1);
+                s = s.substring(0, sL-1);
             } else {
                 s = s.substring(0, a) + s.substring(a+1, s.length());
             }
-            
             System.out.println(s);
             cnt--;
-            if (cnt==1) break;
-        } 
+        }
+        
     }   
 
 }
