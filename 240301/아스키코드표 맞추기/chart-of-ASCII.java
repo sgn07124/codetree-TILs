2f6fa19e769d30d7a2a75bec178fee99ee98fac4
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-03-01 23:29:30 +0900

    Title: 아스키코드표 맞추기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int arr [] = new int [5];
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            char a;
+            a = (char)(arr[i]);
+            System.out.print(a + " ");
+        }
+    }
+}
