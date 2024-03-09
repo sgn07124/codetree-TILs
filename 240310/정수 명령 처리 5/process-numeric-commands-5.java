6e5783aa0607447896a9da45659054a934c48039
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2024-03-10 02:07:30 +0900

    Title: 정수 명령 처리 5, Time: 450ms, Memory: 24MB, Status: Passed - CodeTree

--- 240310/정수 명령 처리 5/process-numeric-commands-5.java
+++ 240310/정수 명령 처리 5/process-numeric-commands-5.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
